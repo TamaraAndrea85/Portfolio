bd48030d591dccca0fbd0241baa9a8906319c781
tamaraandrea <tamara18_1985@msn.com>
2022-03-17 19:06:18 -0400

    added some education

--- src/Education.js
+++ src/Education.js
@@ -8,9 +8,7 @@ export default function Education() {
         <div className="education-one my-4 justify-content-between d-flex flex-row">
           <div className="d-flex flex-column new-one">
             <span className="university-name">General Assembly </span>
-            <span className="university-degree">
-              Bachelors of Technology in Computer Science{" "}
-            </span>
+            <span className="university-degree"> </span>
           </div>{" "}
           <div>
             <span className="year-passedout">2021-2022 </span>
@@ -18,15 +16,13 @@ export default function Education() {
         </div>
         <div className="education-one my-4 justify-content-between d-flex flex-row">
           <div className="d-flex flex-column new-one">
-            <span className="university-name">
-              Jawaharlal Nehru Institute of Technology{" "}
-            </span>
+            <span className="university-name">Onondaga Community College</span>
             <span className="university-degree">
-              Bachelors of Technology in Computer Science{" "}
+              Physical Therapy/Therapist Assistant{" "}
             </span>
           </div>{" "}
           <div>
-            <span className="year-passedout">2017-2021 </span>
+            <span className="year-passedout">2007-2009 </span>
           </div>
         </div>
         <div className="education-one my-4 justify-content-between d-flex flex-row">
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -8,12 +8,12 @@ export default function Testimonials() {
       text: "Tamara is very well liked by peers and instructors, her infectious outgoing personality has the ability to life any mood in any setting. It has be an honor to work with her and to be able to call her not only a well respected engineer, but a new friend. I am sure any company would see how much of a valuable asset she will be.",
     },
     {
-      name: "Vishnu",
-      text: "This guy is incredibly awesome, I hired him and when He delivered, I honestly fell in love with the project. He is indeed a great guy.",
+      name: "Destiny Cerlanek ",
+      text: "I have worked with Tamara for the past 4 years in a skilled nursing rehabilitation therapy department.  Tamara has excellent communication skills along with being a team player. She is extremely organized, reliable, and is flexible enough to work on any project that is assigned to her. In addition, she is able to work independently and is able to follow through to ensure the job gets done. Tamara would be a tremendous asset for your company and has my highest recommendation.",
     },
     {
-      name: "Abhishek",
-      text: "This guy is incredibly awesome, I hired him and when He delivered, I honestly fell in love with the project. He is indeed a great guy.",
+      name: "",
+      text: "",
     },
   ];
   return (
