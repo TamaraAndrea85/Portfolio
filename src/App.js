3479e8e2bdc64cba60c74562124edea5f8f59e9e
tamaraandrea <tamara18_1985@msn.com>
2022-04-10 12:04:54 -0400

    resume

--- src/App.js
+++ src/App.js
@@ -157,7 +157,7 @@ export default function App() {
               <img
                 className="img-fluid custom-img"
                 alt="heroimage"
-                src="https://res.cloudinary.com/dsruc2myl/image/upload/v1646016535/personal/nnaknrzzy1wpgbo3jypj.jpg"
+                src="https://docs.google.com/document/d/e/2PACX-1vSKcDz2KCsv9y3Wu-nWPdju8uUGW9cNki-miybv8lmSXChfW_GV-GWNXmXSi-Drf8PQGpzqM7g9vHBp/pub"
               />
             </div>
           </div>
