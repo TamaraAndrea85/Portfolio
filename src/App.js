20e6a1eefc233eebf6382f563e856693fe1f55e7
tamaraandrea <tamara18_1985@msn.com>
2023-04-18 17:38:47 -0400

    changed resume link

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ export default function App() {
                   Hire me
                 </a>
                 <a
-                  href="https://docs.google.com/document/d/1JZ-4GNpS1CjM_Z2W5IurUg6o8uIqs6tlw-PUca4oS0s/edit"
+                  href="https://docs.google.com/document/d/1Dl9-LePRvmeaZaJhv424PNKUVWYOgtPE/edit"
                   className="btn-resume-me"
                   target=""
                 >
